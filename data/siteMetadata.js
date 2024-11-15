7cdfa91251f2f8c30fe323fd97a092cc98f046f6
hmT <hamityilmaz@outlook.com>
2024-11-15 21:34:09 +0300

    Add who i am blog post

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,9 +1,9 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  title: 'Hamit YILMAZ',
+  author: 'Hamit YILMAZ',
+  headerTitle: 'Hamit YILMAZ Blog',
+  description: 'A blog about Stoizm, Financal Freedom, Software Engineer',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
