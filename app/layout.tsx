4e4d38014ac3c71a7989e196808f428d9ea386d6
hmT <hamityilmaz@outlook.com>
2024-12-05 09:17:27 +0300

    Add favicons

--- app/layout.tsx
+++ app/layout.tsx
@@ -69,8 +69,54 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     >
       <link
         rel="apple-touch-icon"
+        sizes="57x57"
+        href={`${basePath}/static/favicons/apple-icon-57x57.png`}
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="60x60"
+        href={`${basePath}/static/favicons/apple-icon-60x60.png`}
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="72x72"
+        href={`${basePath}/static/favicons/apple-icon-72x72.png`}
+      />
+      <link
+        rel="apple-touch-icon"
         sizes="76x76"
-        href={`${basePath}/static/favicons/apple-touch-icon.png`}
+        href={`${basePath}/static/favicons/apple-icon-76x76.png`}
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="114x114"
+        href={`${basePath}/static/favicons/apple-icon-114x114.png`}
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="120x120"
+        href={`${basePath}/static/favicons/apple-icon-120x120.png`}
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="144x144"
+        href={`${basePath}/static/favicons/apple-icon-144x144.png`}
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="152x152"
+        href={`${basePath}/static/favicons/apple-icon-152x152.png`}
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href={`${basePath}/static/favicons/apple-icon-180x180.png`}
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="192x192"
+        href={`${basePath}/static/favicons/android-icon-192x192.png`}
       />
       <link
         rel="icon"
@@ -81,14 +127,19 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <link
         rel="icon"
         type="image/png"
+        sizes="96x96"
+        href={`${basePath}/static/favicons/favicon-96x96.png`}
+      />
+      <link
+        rel="icon"
+        type="image/png"
         sizes="16x16"
         href={`${basePath}/static/favicons/favicon-16x16.png`}
       />
       <link rel="manifest" href={`${basePath}/static/favicons/site.webmanifest`} />
-      <link
-        rel="mask-icon"
-        href={`${basePath}/static/favicons/safari-pinned-tab.svg`}
-        color="#5bbad5"
+      <meta
+        name="msapplication-TileImage"
+        content={`${basePath}/static/favicons/ms-icon-144x144.png`}
       />
       <meta name="msapplication-TileColor" content="#000000" />
       <meta name="theme-color" media="(prefers-color-scheme: light)" content="#fff" />
